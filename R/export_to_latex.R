ff917756a4b448a4c405bffc5d92eb16e8b1c90c
TomKellyGenetics <tomkellygenetics@gmail.com>
2016-04-05 16:48:47 +1200

    inialise R package git repo

---
+++ R/export_to_latex.R
@@ -0,0 +1,40 @@
+#' An Export Function to produce a LaTeX table
+#'
+#' Exports the R data object into a tex file with the same name as in the R working environment (into the current working directory). Automatically detects integers, strings, and numerics automatically formatting accordingly as string or numerical (with significant figures). Allows formatting of spaces in rownames.
+#' @param x Data matrix or Data Frame to be exported into a tex table.
+#' @keywords dataframe, matrix, LaTeX, tex, table
+#' @export
+#' @examples
+#' export_to_latex()
+#' export_to_latex(example)
+#' % latex table generated in R 3.2.4 by xtable 1.8-2 package
+#' % Tue Apr  5 16:40:56 2016
+#' \begin{table}[ht]
+#' \centering
+#' \begin{tabular}{rrll}
+#' \hline
+#' & c.1..2..3. & c..hello....hey....hi.. & c.T..F..T. \\
+#' \hline
+#' 1 &   1 & hello & TRUE \\
+#' 2 &   2 & hey & FALSE \\
+#' 3 &   3 & hi & TRUE \\
+#' \hline
+#' \end{tabular}
+#' \end{table}
+export_to_latex <-
+function(x){
+  df <- as.data.frame(x)
+  rownames(df) <- make.unique(as.character(rownames(df)), sep="_")
+  clx <- unlist(lapply(df, class))
+  disp <- rep(NULL, length(clx))
+  for(ii in 1:length(clx)){
+    if(clx[ii] == "numeric") disp[ii] <- "g"
+    if(clx[ii] == "complex") disp[ii] <- "g"
+    if(clx[ii] == "integer") disp[ii] <- "d"
+    if(clx[ii] == "factor") disp[ii] <- "s"
+    if(clx[ii] == "character") disp[ii] <- "s"
+    if(clx[ii] == "logical") disp[ii] <- "s"
+  }
+  disp <- c("s", disp) #add rownames
+  print(xtable(df, display=disp), type="latex", file=paste0(deparse(substitute(x)), ".tex"))
+}
---
+++ R/export_to_latex_loop.R
@@ -0,0 +1,16 @@
+#' An Export Function to produce LaTeX tables for many objects simultaneously
+#'
+#' Calls \code{\link[R2LaTeX]{export_to_latex}} for every objects given. Exports the R data object into a tex file with the same name as in the R working environment (into the current working directory). Automatically detects integers, strings, and numerics automatically formatting accordingly as string or numerical (with significant figures). Allows formatting of spaces in rownames.
+#' @param object_list string or character vector. Names of objects (data matrices or data frames) to be exported as LaTeX tables with names assigned in the R working environment.
+#' @keywords dataframe, matrix, LaTeX, tex, table
+#' @export
+#' @examples
+#' export_to_latex_loop()
+#'
+export_to_latex_loop <-
+function(object_list){
+  for(command in paste0('export_SL_latex(', object_list, ")")){
+    eval(parse(text=command))
+    print(command)
+  }
+}
